ca6e9d18d36f2e0d01931ad2ef64f4ad1dc24235
Hossamesmail2003 <hossam02003@gmail.com>
2024-09-24 20:32:39 +0300

    edit on admin site search,filter

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,9 @@ from .models import Post,Comment
 # Register your models here.
 class PostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
     summernote_fields = '__all__'
+    list_display =['title','draft','author']
+    list_filter = ['draft','author']
+    search_fields = ['title','content']
 
 admin.site.register(Post,PostAdmin)
 admin.site.register(Comment)
