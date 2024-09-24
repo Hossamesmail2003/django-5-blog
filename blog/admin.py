a07a8b24375f98322ba76fb8846ace27e7c80a64
Hossamesmail2003 <hossam02003@gmail.com>
2024-09-24 20:19:51 +0300

    add summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post,Comment
 
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Comment)
